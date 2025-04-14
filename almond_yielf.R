6c081b3ea75ed0089ab6628786385f704c1af8ee
kaimorquecho <kaimorquecho@ucsb.edu>
2025-04-13 23:06:46 -0700

    improved variable names to be more meaningful

--- almond_yielf.R
+++ almond_yielf.R
@@ -1,13 +1,22 @@
 
-almond_yield_model <- function(Tn2, P1) {
+#' Almond Yield Anomaly Model
+#' models almond yield anomaly response to climate using time series data
+#' @param feb_tmin is February's minimum temperature in C°
+#' @param jan_precip is total precipitation in January in mm
+#'
+#' @returns a list of the minimum, maximum, and mean almond yield anomaly results in ton/acre
+#' @author Kaiju Morquecho
+#' 
+
+almond_model <- function(feb_tmin, jan_precip) {
  
-  yield_anomaly = -0.015 * Tn2 - 0.0046 * Tn2^2 - 0.07 * P1 + 0.0043 * P1^2 + 0.28
+  almond_yield_anomaly = -0.015 * feb_tmin - 0.0046 * feb_tmin^2 - 0.07 * jan_precip + 0.0043 * jan_precip^2 + 0.28
   
- output <- list(
-   max_almond_yield_anomaly = max(yield_anomaly),
-   min_almond_yield_anomaly = min(yield_anomaly),
-   mean_almond_yield_anomaly = mean(yield_anomaly))
+ anomaly_summary <- list(
+   max_almond_yield_anomaly = max(almond_yield_anomaly),
+   min_almond_yield_anomaly = min(almond_yield_anomaly),
+   mean_almond_yield_anomaly = mean(almond_yield_anomaly))
    
    
-  return(output)
+  return(anomaly_summary)
 }
